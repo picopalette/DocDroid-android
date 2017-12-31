9b051811fff39b0f762204e56893e7ca1b5e002d
caped-speedster <aswinsundar17@gmail.com>
2018-01-01 00:07:34 +0530

    Completed the card for heart-pain & accident and their respective dialogs.  Happy New Year my Dear Piconians...

--- app/src/main/java/io/picopalette/apps/docdroid/Fragments/HomeFragment.java
+++ app/src/main/java/io/picopalette/apps/docdroid/Fragments/HomeFragment.java
@@ -3,9 +3,11 @@ package io.picopalette.apps.docdroid.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import io.picopalette.apps.docdroid.R;
 
@@ -14,6 +16,9 @@ import io.picopalette.apps.docdroid.R;
  */
 
 public class HomeFragment extends Fragment {
+
+    private LinearLayout heartpainLayout;
+    private LinearLayout accidentLayout;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -26,8 +31,32 @@ public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+        heartpainLayout = view.findViewById(R.id.reportHeartPainLayout);
+        accidentLayout = view.findViewById(R.id.reportAccidentLayout);
+        heartpainLayout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                AlertDialog.Builder mB = new AlertDialog.Builder(getContext());
+                View alertView = inflater.inflate(R.layout.dialog_heartpain,null);
+                mB.setView(alertView);
+                final AlertDialog dialog = mB.create();
+                dialog.show();
+                return true;
+            }
+        });
+        accidentLayout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                AlertDialog.Builder mB = new AlertDialog.Builder(getContext());
+                View alertView = inflater.inflate(R.layout.dialog_accident,null);
+                mB.setView(alertView);
+                final AlertDialog dialog = mB.create();
+                dialog.show();
+                return true;
+            }
+        });
         return view;
     }
 }
