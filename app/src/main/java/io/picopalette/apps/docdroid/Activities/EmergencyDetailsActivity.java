42f7a25fde19941beca0e237663e287cf856032b
caped-speedster <aswinsundar17@gmail.com>
2018-01-05 11:00:56 +0530

    Emergency Details Activity and To Do's in home fragment

--- app/src/main/java/io/picopalette/apps/docdroid/Activities/EmergencyDetailsActivity.java
+++ app/src/main/java/io/picopalette/apps/docdroid/Activities/EmergencyDetailsActivity.java
@@ -2,6 +2,7 @@ package io.picopalette.apps.docdroid.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import io.picopalette.apps.docdroid.R;
 
@@ -11,6 +12,8 @@ public class EmergencyDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_details);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
 }
