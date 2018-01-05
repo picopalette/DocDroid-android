a071909dd7ff6133202b57c3f69d574a1cabd38f
caped-speedster <aswinsundar17@gmail.com>
2018-01-05 23:17:38 +0530

    Notification - ConfirmEmergencyActivity

---
+++ app/src/main/java/io/picopalette/apps/docdroid/activities/ConfirmEmergencyActivity.java
@@ -0,0 +1,15 @@
+package io.picopalette.apps.docdroid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.picopalette.apps.docdroid.R;
+
+public class ConfirmEmergencyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_emergency);
+    }
+}
