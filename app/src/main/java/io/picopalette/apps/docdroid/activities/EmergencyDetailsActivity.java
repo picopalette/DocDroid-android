f026a8b1cc9e6ae9aaae63d2a5f528d5b91f88a9
ramkumar <ramukmar1998@gmail.com>
2018-01-05 12:03:55 +0530

    Resolved conflict

--- app/src/main/java/io/picopalette/apps/docdroid/activities/EmergencyDetailsActivity.java
+++ app/src/main/java/io/picopalette/apps/docdroid/activities/EmergencyDetailsActivity.java
@@ -1,21 +1,28 @@
 package io.picopalette.apps.docdroid.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.WindowManager;
-
 import io.picopalette.apps.docdroid.R;
+
+/**
+ * Created by ramkumar on 05/01/18.
+ */
 
 public class EmergencyDetailsActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                 | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
                 | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-                | WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
     }
 }
